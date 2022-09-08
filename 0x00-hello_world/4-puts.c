17c0d71b004bfc81b706aa1845cab481cd581bde
Osaclay <osaosun@gmail.com>
2022-09-08 13:13:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*main - the entry point
+*
+*Return: always 0 after printing the function
+*/
+int main(void)
+{puts("\"PProgramming is like building a multilingual puzzle");
+        return (0);
+}
