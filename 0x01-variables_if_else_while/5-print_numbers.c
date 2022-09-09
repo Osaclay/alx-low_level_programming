353f60b344696017fd0d3efb6a7505ca9f75d25a
Osaclay <osaosun@gmail.com>
2022-09-09 15:42:57 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Print all single numbers of base 10.
+* Return: 0
+*/
+
+int main(void)
+{
+char letra;
+
+for (letra = '0' ; letra <= '9' ; letra++)
+{
+putchar(letra);
+}
+putchar(10);
+return (0);
+}
