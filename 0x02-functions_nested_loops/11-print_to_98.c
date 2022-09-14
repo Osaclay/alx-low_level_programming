1732757796079390fcf55bd8ca2dfe7fe45232a8
Osaclay <osaosun@gmail.com>
2022-09-13 20:45:35 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,8 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
-* print_to_98 - description
+* print_to_98 - prints all natural numbers from n to 98
 * @n: number
 */
-
 void print_to_98(int n)
 {
 for (; n > 98; n--)
