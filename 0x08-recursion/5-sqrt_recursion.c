153d08600690ba90c155554c120df50143d74734
Osaclay <osaosun@gmail.com>
2022-09-28 07:40:46 -0700

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* _sqrt_nat - a function that calculates
+* the natural square root of a number
+* @n: Number raw.
+* @r: Result.
+* Return: Result.
+*/
+
+int _sqrt_nat(int n, int r)
+{
+if (n < 0)
+return (-1);
+
+if (n == 0)
+return (0);
+
+if (n == 1)
+return (1);
+
+if (r > n / 2)
+return (-1);
+
+if (n != r * r)
+return (_sqrt_nat(n, r + 1));
+
+if (n == r * r)
+return (r);
+
+return (-1);
+}
+
+/**
+* _sqrt_recursion - funtion that calculates
+* the natural square root of a number
+* @n: Number.
+* Return: Result.
+*/
+
+int _sqrt_recursion(int n)
+{
+return (_sqrt_nat(n, 0));
+}
