e13a275d0727982d24f65d6167c3959d7076043f
Osaclay <osaosun@gmail.com>
2022-10-10 05:59:12 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,157 +1,79 @@
-nclude <stdlib.h>
-
+#include <stdlib.h>
 #include "dog.h"
 
-
-
 /**
- *
- *  * _strlen - a function that counts the length of a string.
- *
- *   * @s: String.
- *
- *    * Return: Length.
- *
- *     */
-
-
+* _strlen - a function that counts the length of a string.
+* @s: String.
+* Return: Length.
+*/
 
 unsigned int _strlen(char *s)
-
 {
+unsigned int c;
 
-	unsigned int c;
+for (c = 0; s[c]; c++)
+;
 
-
-
-	for (c = 0; s[c]; c++)
-
-		;
-
-
-
-	return (c);
-
+return (c);
 }
 
-
-
 /**
- *
- *  * _strcpy - A function that copies a strings.
- *
- *   * @s: String.
- *
- *    * Return: New copy string.
- *
- *     */
-
-
+* _strcpy - A function that copies a strings.
+* @s: String.
+* Return: New copy string.
+*/
 
 char *_strcpy(char *s)
-
 {
+char *copy = NULL;
+unsigned int size, i;
 
-	char *copy = NULL;
+/*Return NULL if s is empty*/
+if (s == NULL)
+return (NULL);
+/*Allocate memory*/
+size = _strlen(s);
 
-	unsigned int size, i;
+copy = malloc(sizeof(char) * (size + 1));
 
+if (copy == NULL)
+return (NULL);
 
+/*Copy string*/
+for (i = 0; s[i] != '\0'; i++)
+copy[i] = s[i];
+copy[i] = '\0';
 
-	/*Return NULL if s is empty*/
-
-	if (s == NULL)
-
-		return (NULL);
-
-	/*Allocate memory*/
-
-	size = _strlen(s);
-
-
-
-	copy = malloc(sizeof(char) * (size + 1));
-
-
-
-	if (copy == NULL)
-
-		return (NULL);
-
-
-
-	/*Copy string*/
-
-	for (i = 0; s[i] != '\0'; i++)
-
-		copy[i] = s[i];
-
-	copy[i] = '\0';
-
-
-
-	return (copy);
-
+return (copy);
 }
 
-
-
 /**
- *
- *  * new_dog - A function that creates a new dog.
- *
- *   * @name: Dog's name.
- *
- *    * @age: Dog's age.
- *
- *     * @owner: Dog's owner
- *
- *      * Return: New dog.
- *
- *       */
-
-
+* new_dog - A function that creates a new dog.
+* @name: Dog's name.
+* @age: Dog's age.
+* @owner: Dog's owner
+* Return: New dog.
+*/
 
 dog_t *new_dog(char *name, float age, char *owner)
-
 {
+/*Create a pointer to new structure*/
+dog_t *my_new_dog = malloc(sizeof(dog_t));
 
-	/*Create a pointer to new structure*/
+if (my_new_dog == NULL)
+return (my_new_dog);
 
-	dog_t *my_new_dog = malloc(sizeof(dog_t));
+/*Inicialize structure*/
+my_new_dog->name = _strcpy(name);
+my_new_dog->age = age;
+my_new_dog->owner = _strcpy(owner);
 
-
-
-	if (my_new_dog == NULL)
-
-		return (my_new_dog);
-
-
-
-	/*Inicialize structure*/
-
-	my_new_dog->name = _strcpy(name);
-
-	my_new_dog->age = age;
-
-	my_new_dog->owner = _strcpy(owner);
-
-
-
-	if (my_new_dog->name == NULL || my_new_dog->owner == NULL)
-
-	{
-
-		free(my_new_dog->name);
-
-		free(my_new_dog->owner);
-
-		free(my_new_dog);
-
-		return (NULL);
-
-	}
-
-	return (my_new_dog);
-
+if (my_new_dog->name == NULL || my_new_dog->owner == NULL)
+{
+free(my_new_dog->name);
+free(my_new_dog->owner);
+free(my_new_dog);
+return (NULL);
+}
+return (my_new_dog);
 }
