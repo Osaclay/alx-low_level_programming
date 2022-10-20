d58fdb66383e8e85595e7b39a08975422709191e
Osaclay <osaosun@gmail.com>
2022-10-20 14:18:31 -0700

    singly linked lists project

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,50 +1,30 @@
-#include <stddef.h>
 #include "lists.h"
-#include <string.h>
-#include <stdlib.h>
 
 /**
-* _strlen - This function returns the length of a string
-* @s: Variable that contains a character array to be counted
-* Return: Length of the string
-*/
-
-int _strlen(const char *s)
-{
-int i;
-
-for (i = 0; s[i] != '\0'; i++)
-continue;
-
-return (i);
-}
-
-/**
-* *add_node - This function adds a new node at the
-* beginning of a linked list_t list
-* @head: The beginning of the linked list
-* @str: The string to duplicate into the new node
-* Return: The address of the new element, of NULL if it failed
+* add_node - adds a new node at the beginning
+* of a list_t list.
+* @head: head of the linked list.
+* @str: string to store in the list.
+* Return: address of the head.
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
+list_t *new;
+size_t nchar;
 
-/* create new list & allocate memory for it */
-list_t *new = (list_t *)malloc(sizeof(list_t));
-
+new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
 
-/* assign new data */
 new->str = strdup(str);
-new->len = _strlen(str);
 
-/* assign 'head' as next element of new list */
-new->next = (*head);
+for (nchar = 0; str[nchar]; nchar++)
+;
 
-/* move head to point to new list */
-(*head) = new;
+new->len = nchar;
+new->next = *head;
+*head = new;
 
-return (new);
-
+return (*head);
+}
